5731e21f6441eb0f88128826b44d26d9241c513d
Chris Choi <chrisb.choi@gmail.com>
2019-11-18 20:11:33 +1100

    remove unused section

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,31 +3,19 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import {
-  GlobalDispatchContext,
-  GlobalStateContext,
-} from "../context/GlobalContextProvider"
+import { GlobalStateContext } from "../context/GlobalContextProvider"
 
 const SecondPage = () => {
-  const dispatch = useContext(GlobalDispatchContext)
   const state = useContext(GlobalStateContext)
 
   return (
     <Layout>
-      <SEO title="Page two" />
+      <SEO title='Page two' />
       <h1>Hi from the second page</h1>
       <p>Welcome to page 2</p>
       <span>{state.theme}</span>
-      <button
-        type="button"
-        onClick={() => {
-          dispatch({ type: "TOGGLE_THEME" })
-        }}
-      >
-        Toggle Theme
-      </button>
 
-      <Link to="/">Go back t}o the homepage</Link>
+      <Link to='/'>Go back t}o the homepage</Link>
     </Layout>
   )
 }
